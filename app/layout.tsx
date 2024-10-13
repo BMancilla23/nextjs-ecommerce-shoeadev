23b0b13518f801fdbdad1a7c5bbea11350fdf8a0
Bryan Mancilla <bmancilla119@gmail.com>
2024-10-12 23:14:54 -0500

    rafactor: updated code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,16 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+/* import localFont from "next/font/local"; */
 import "./globals.css";
+import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
+import { extractRouterConfig } from "uploadthing/server";
+import { ourFileRouter } from "./api/uploadthing/core";
+import { Inter } from "next/font/google";
 
-const geistSans = localFont({
+const inter = Inter({
+  subsets: ["latin"],
+});
+
+/* const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
@@ -11,7 +19,7 @@ const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
-});
+}); */
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,10 +32,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="es" suppressHydrationWarning={true}>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        suppressHydrationWarning={true}
+        /* className={`${geistSans.variable} ${geistMono.variable} antialiased`} */
+        className={inter.className}
       >
+        <NextSSRPlugin routerConfig={extractRouterConfig(ourFileRouter)} />
         {children}
       </body>
     </html>
