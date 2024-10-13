061a49079eb53aee4140e20b391a8d02745b2ab6
Bryan Mancilla <bmancilla119@gmail.com>
2024-10-12 23:31:41 -0500

    chore: categories for use of options

---
+++ app/lib/categories.ts
@@ -0,0 +1,17 @@
+export const categories = [
+  {
+    id: 0,
+    title: "Hombre",
+    name: "men",
+  },
+  {
+    id: 1,
+    title: "Mujer",
+    name: "women",
+  },
+  {
+    id: 2,
+    title: "Niños",
+    name: "kids",
+  },
+];
