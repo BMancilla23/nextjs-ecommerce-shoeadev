5dadb6e23f47dfb50cb6372916c40277357017d5
Bryan Mancilla <bmancilla119@gmail.com>
2024-10-12 23:34:07 -0500

    feat: products page completed

---
+++ app/(store)/products/[name]/page.tsx
@@ -0,0 +1,108 @@
+import { ProductCard } from "@/app/components/store/ProductCard";
+import prisma from "@/app/lib/db";
+import { notFound } from "next/navigation";
+
+async function getData(productCategory: string) {
+  switch (productCategory) {
+    case "all": {
+      const data = await prisma.product.findMany({
+        select: {
+          name: true,
+          images: true,
+          price: true,
+          id: true,
+          description: true,
+        },
+        where: {
+          status: "published",
+        },
+      });
+
+      return {
+        title: "All Products",
+        data: data,
+      };
+    }
+    case "men": {
+      const data = await prisma.product.findMany({
+        select: {
+          name: true,
+          images: true,
+          price: true,
+          id: true,
+          description: true,
+        },
+        where: {
+          category: "men",
+          status: "published",
+        },
+      });
+
+      return {
+        title: "Productos para hombres",
+        data: data,
+      };
+    }
+    case "women": {
+      const data = await prisma.product.findMany({
+        select: {
+          name: true,
+          images: true,
+          price: true,
+          id: true,
+          description: true,
+        },
+        where: {
+          category: "women",
+          status: "published",
+        },
+      });
+
+      return {
+        title: "Productos para mujeres",
+        data: data,
+      };
+    }
+    case "kids": {
+      const data = await prisma.product.findMany({
+        select: {
+          name: true,
+          images: true,
+          price: true,
+          id: true,
+          description: true,
+        },
+        where: {
+          category: "kids",
+          status: "published",
+        },
+      });
+
+      return {
+        title: "Productos para niños",
+        data: data,
+      };
+    }
+
+    default:
+      return notFound();
+  }
+}
+
+export default async function ProductCategoriesPage({
+  params,
+}: {
+  params: { name: string };
+}) {
+  const { data, title } = await getData(params.name);
+  return (
+    <section>
+      <h1 className="font-semibold text-3xl my-5">{title}</h1>
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
+        {data.map((item) => (
+          <ProductCard item={item} key={item.id} />
+        ))}
+      </div>
+    </section>
+  );
+}
